bf39568da6f74b8a50e8708c6b437c89b004e7b5
david short <sho13014@byui.edu>
2016-01-25 09:36:23 -0700

    now says hello github world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
+/*
+ * Collaborators: Cam and Ben
+ */
+
 public class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello GitHub World");
 	}
 }
